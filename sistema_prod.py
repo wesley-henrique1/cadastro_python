57e97f0fbfd76adc592130fd15bf1d6269765735
Wesley Henrique <wesleyhfo123@gmail.com>
2025-10-15 22:49:58 -0300

    adicionando escolhas no menu() e finalizando cadastro()

--- sistema_prod.py
+++ sistema_prod.py
@@ -1,41 +1,4 @@
-
-
-
-def main():
-    def menu():
-        
-        pass
-    def cadastrar(COD, DESC, FATOR, EMB, END):
-        novo_registro = {"COD": COD, "DESCRICAO": DESC, "FATOR": FATOR, "EMBALAGEM": EMB, "ENDERECO": END}
-        return base_dados.append(novo_registro)
-    def listar():
-        pass
-    def buscar():
-        pass
-    def remover():
-        pass
-    def atualizar():
-        pass
-
-    base_dados = [
-        {"COD": 101000, "DESCRICAO": "SANDALIA MASC 37/8", "FATOR": 12, "EMBALAGEM": "UN/001/UN", "ENDERECO": "30-10-1-101"},
-        {"COD": 100100, "DESCRICAO": "CHOC TALENTO DARCK", "FATOR": 6, "EMBALAGEM": "DP/012/UN", "ENDERECO": "11-50-1-202"},
-        {"COD": 987654, "DESCRICAO": "TV AOC 32", "FATOR": 1, "EMBALAGEM": "UN/001/UN", "ENDERECO": "01-64-1-102"}
-    ]
-
-
-    # CADASTRO
-    cod_list = []
-    tt_cod_list = len(cod_list)
-    for produto in base_dados:
-        if tt_cod_list != 0:
-            print(f"lista cheia {cod_list}")
-        else:
-            continue
-        cod_list.append(produto["COD"])
-
 def validar_erro(e):
-    print("=" * 60)
     if isinstance(e, KeyError):
         return f"KeyError: A coluna ou chave '{e}' não foi encontrada."
     elif isinstance(e, PermissionError):
@@ -46,6 +9,91 @@ def validar_erro(e):
         return f"ValueError: Erro de valor. Mensagem original: {e}"
     else:
         return f"Ocorreu um erro inesperado: {e}"
+
+
+
+def main():
+    base_dados = [
+        {"COD": 80000, "DESCRICAO": "SANDALIA MASC 45/6", "FATOR": 12, "EMBALAGEM": "UN/001/UN", "ENDERECO": "30-10-1-101", "PRECO": 39.59},
+        {"COD": 80002, "DESCRICAO": "CHOC TALENTO DARCK WHITE", "FATOR": 6, "EMBALAGEM": "DP/012/UN", "ENDERECO": "11-50-1-202", "PRECO": 56.99},
+        {"COD": 80001, "DESCRICAO": "TV AOC 32", "FATOR": 1, "EMBALAGEM": "UN/001/UN", "ENDERECO": "01-64-1-102", "PRECO": 1578.59}
+        ]
+
+    def menu():
+        
+        print("=" *60 + "sistema" + "=" *60)
+        print("\nBem vindo ao sistema\n")
+    try:
+        while True:
+            print("MENU")
+            print("1 - cadastro")
+            print("2 - lista")
+            print("0 - sair")
+
+            escolha = int(input("\nDigite o numero da opção desejada: \n"))
+
+            if escolha == 1:
+                print("\nFavor informar os campos abaixo\n")
+                desc = input("descrição: ")
+                fator = int(input("fator de converção: "))
+                emb = input("Embalagem: ")
+                end = input("Endereço:")
+                vl = float(input("valor do produto:"))
+
+                cadastrar(desc, fator, emb, end, vl)   
+            elif escolha == 2:
+                listar()   
+            elif escolha == 0:
+                print("\n\nObrigado por utilizar o sistema")
+                break
+            else:
+                print("Numero invalido valor digitar o numero correspodente a opção desejada")          
+    except Exception as e:
+        error = validar_erro(e)
+        print(f"cadastro: {error}\n")
+        
+
+
+
+    def cadastrar(desc, fator, emb, end, vl):
+        try:
+            cod_list = []
+            count_list = len(cod_list)
+            
+            for produto in base_dados:
+                if count_list != 0:
+                    print(f"lista cheia {cod_list}")
+                else:
+                    cod_list.append(produto["COD"])
+                maior_cod = max(cod_list)
+                cod = maior_cod + 1
+
+            print("aqui",cod_list)
+            print("aqui",cod)
+
+            novo_registro = {
+                "COD": cod,
+                "DESCRICAO": desc,
+                "FATOR": fator,
+                "EMBALAGEM": emb,
+                "ENDERECO": end,
+                "PRECO": vl
+            }
+        except Exception as e:
+            error = validar_erro(e)
+            print(f"cadastro: {error}\n")
+            
+        return base_dados.append(novo_registro)
+    def listar():
+        pass
+    def buscar():
+        pass
+    def remover():
+        pass
+    def atualizar():
+        pass
+
+    menu()
 
 
 if __name__ == "__main__":
